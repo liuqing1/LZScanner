f23a2d8857b26d5b52278fed6e2554b8f9bde652
liuqing <liuqing@appledeMacBook-Pro-2.local>
2018-05-15 15:31:45 +0800

    更新

---
+++ LZScaner/CustomViewController.h
@@ -0,0 +1,17 @@
+//
+//  CustomViewController.h
+//  LZScanner
+//
+//  Created by liuqing on 2018/5/15.
+//  Copyright © 2018年 liuqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomViewController : UIViewController
+
+
+-(void)test;
+
+
+@end
---
+++ LZScaner/CustomViewController.m
@@ -0,0 +1,37 @@
+//
+//  CustomViewController.m
+//  LZScanner
+//
+//  Created by liuqing on 2018/5/15.
+//  Copyright © 2018年 liuqing. All rights reserved.
+//
+
+#import "CustomViewController.h"
+
+@interface CustomViewController ()
+
+@end
+
+@implementation CustomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
